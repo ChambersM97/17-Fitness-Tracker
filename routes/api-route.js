dc81065c0bb85decb483e7c1638985fa75217395
Chambers Mansour <chambersmansour97@gmail.com>
2020-11-21 13:45:42 -0600

    added readmetemplate

--- routes/api-route.js
+++ routes/api-route.js
@@ -18,16 +18,16 @@ router.get("/api/workouts", (req, res) => {
 // }
 
 //adds exerises to a previous workout plan
-router.post("/api/workouts", (req, res) => {
-    db.Workout.insertMany(req.body, (error, data) => {
-        console.log(req.body);
-        if (error) {
-            res.send(error);
-        } else {
-            res.send(data);
-        }
-    })
-})
+// router.post("/api/workouts", (req, res) => {
+//     db.Workout.insertMany(req.body, (error, data) => {
+//         console.log(req.body);
+//         if (error) {
+//             res.send(error);
+//         } else {
+//             res.send(data);
+//         }
+//     })
+// })
 
 
 // router.post('/api/workouts', (req, res) => {
--- routes/html-route.js
+++ routes/html-route.js
@@ -1,10 +1,16 @@
 const router = require("express").Router();
 const path = require("path");
 
+router.get("/", (req, res) => {
+    // console.log(__dirname);
+    res.sendFile(path.join(__dirname,"../public/index.html"))
+})
+
 router.get("/exercise", (req, res) => {
     // console.log(__dirname);
     res.sendFile(path.join(__dirname,"../public/exercise.html"))
 })
+
 router.get("/stats", (req, res) => {
         res.sendFile(path.join(__dirname,"../public/stats.html"))
 })
