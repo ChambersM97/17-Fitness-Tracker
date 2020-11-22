a406273008d8164d0c2aefcfa85de83627212d3d
Chambers Mansour <chambersmansour97@gmail.com>
2020-11-21 22:25:23 -0600

    updated api routes

--- models/workout.js
+++ models/workout.js
@@ -12,27 +12,38 @@ const workoutSchema = new Schema(
             {
                 type: {
                     type: String,
+                    trim: true,
                     required: "Choose a exercise."
                 },
                 name: {
                     type: String,
+                    trim: true,
                     required: "You must input a name."
                 },
                 duration: {
                     type: Number,
+                    trim: true,
                     required: "Enter an amount"
                 },
                 weight: {
-                    type: Number
+                    type: Number,
+                    trim: true,
+                    required: "Enter an amount"
                 },
                 reps: {
-                    type: Number
+                    type: Number,
+                    trim: true,
+                    required: "Enter an amount"
                 },
                 sets: {
-                    type: Number
+                    type: Number,
+                    trim: true,
+                    required: "Enter an amount"
                 },
                 distance: {
-                    type: Number
+                    type: Number,
+                    trim: true,
+                    required: "Enter an amount"
                 }
             }
         ]
--- routes/api-route.js
+++ routes/api-route.js
@@ -1,37 +1,42 @@
 const router = require("express").Router();
+const { Workout } = require("../models");
 // const { Workout } = require("../models");
 var db = require("../models")
 
-//shows all workouts from the database
 router.get("/api/workouts", (req, res) => {
-    db.Workout.find({}, (error, data) => {
-      if (error) {
-        res.send(error);
-      } else {
-        res.json(data);
-      }
+  Workout.find()
+    .then(data => {
+      res.json(data)
+    })
+    .catch(err => {
+      res.json(err)
     });
+});
+//shows all workouts from the database
+router.post("/api/workouts", (req, res) => {
+    Workout.create({})
+      .then(data => {
+        res.json(data)
+      })
+      .catch(err => {
+        res.json(err)
+        console.log("error!.") 
+      });
+    });
+  
+
+
+router.put("api/workouts/:id", (req, res) => {
+  db.Workout.insertOne({}, (error, data) => {
+    if (error) {
+      res.send(error);
+    } else {
+      res.send(data);
+      console.log("insert works!")
+    }
   });
+})
 
-// router.get("/stats", (req, res) => {
-//     db.Workout.find
-// }
-
-//adds exerises to a previous workout plan
-// router.post("/api/workouts", (req, res) => {
-//     db.Workout.insertMany(req.body, (error, data) => {
-//         console.log(req.body);
-//         if (error) {
-//             res.send(error);
-//         } else {
-//             res.send(data);
-//         }
-//     })
-// })
-
-
-// router.post('/api/workouts', (req, res) => {
-//     db.Workout.find({}, ())
-// })
-
+  
+  
 module.exports = router;
